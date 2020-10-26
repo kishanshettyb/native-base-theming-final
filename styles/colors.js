44eacb4869534248796b911ff238276744992d06
kishanshettyb <kishanqr@gmail.com>
2020-10-26 22:25:00 +0530

    add aign in screen

--- styles/colors.js
+++ styles/colors.js
@@ -1,5 +1,6 @@
 export const black = "#000";
 export const white = "#fff";
+export const gray = "#A3A3A3";
 
 export const primary = "#4136F1";
 export const secondary = "#8543FF";
@@ -9,4 +10,4 @@ export const success = "#18ce0f";
 export const warning = "#FFB236";
 
 export const input = "#F0F0F0";
-export const placeholder = "#909090";
+export const placeholder = "#F7F7F7";
